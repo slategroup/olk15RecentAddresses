2e448f32d9fd02621c5c021485f1ddcdfbfdce41
Greg Lavallee <greg.lavallee@slate.com>
2020-04-29 20:54:33 -0400

    Updates for getting this running again

---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+import getopt
+
+from RecentAddressesParser.parser import Parser
+
+
+def main(argv):
+    inputfile = ''
+    outputfile = ''
+    try:
+        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
+    except getopt.GetoptError:
+        print 'test.py -i <inputfile> -o <outputfile>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'test.py -i <inputfile> -o <outputfile>'
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+
+    parser = Parser(inputfile)
+    parser.go()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
